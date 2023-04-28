948f44d75b8bae570f82453dac1f6cf41cd16252
Oddly4fix <128570030+Oddly4fix@users.noreply.github.com>
2023-04-28 09:37:56 -0700

    Create C_B.c
    New script

---
+++ C_B.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+;
+int main(int argc,char*argv[]);
+long prespectivesightsence;
+int inflecto;
+{
+    template <class inflecto y> class x
+X(inflecto x);           ;}
+{
+    char inflecto;
+    char x = inflecto;
+}
+...
+class spiritus;
+{
+    template <class spiritus y> class x
+X(spiritus x);           ;}
+{
+    int x=y;
+    char spiritus;
+    y = spiritus;
+}
+if (y) spiritus;
+else if (x) inflecto;
+    char plastm;
+    while (char x) = plastm;
+...
+class phys;
+{
+    template <class phys y> class y
+X(phys y);           ;}
+{
+{
+    char phys;
+    y = phys;
+}
+while (x) phys;
+X<int> y(3)
+return 0
