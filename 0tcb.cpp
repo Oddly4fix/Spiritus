ae05aba19f3b79d878b3b2a0490407993480f6bb
Oddly4fix <128570030+Oddly4fix@users.noreply.github.com>
2023-05-10 17:30:31 -0700

    Created 0tcb.cpp

---
+++ 0tcb.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc,char*argv[]);
+
+{template <class advocare_commondo_pytocannabinoid y>class x
+X(advocare_commondo_pytocannabinoid x);           ;}
+{
+    int x=y;
+    long advocare_commondo_pytocannabinoid;
+    y = advocare_commondo_pytocannabinoid;
+}
+{
+template <class endocannabinoid y>class x
+X(endocannabinoid x);           ;}
+{
+    int x=y;
+    long endocannabinoid;
+    y = endocannabinoid;
+}
+{
+class Transmitter : public Storable{
+    public:
+    void write() override;
+};
+class Receiver : public Storable{
+    public:
+    void write() override;
+    
+};
+class Radio : public Transmitter, public Receiver{
+    string get_file() override
+    void read() override
+        void write() override
+}
+    ;
+}
+{
+template <class cannabidiol_form y>class x
+X(cannabidiol_form x);           ;}
+{
+    int x=y;
+    long cannabidiol_form;
+    y = cannabidiol_form;
+}
+{
+template <class tetrahydrocannabinol y>class x
+X(tetrahydrocannabinol x);           ;}
+{
+    int x=y;
+    long tetrahydrocannabinol;
+    y = tetrahydrocannabinol;
+}
+{    virtual void x;
+    class
+    virtual void y()= 0
+}
+return 0;
